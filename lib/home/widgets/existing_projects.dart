e743813beadeef70f788c8db65bc8da303f1850c
deswif <max@wantapps.com>
2022-07-04 10:51:47 +0300

    start 2.0

--- lib/home/widgets/existing_projects.dart
+++ lib/home/widgets/existing_projects.dart
@@ -5,6 +5,6 @@ class ExistingProjectsButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ElevatedButton(onPressed: () {}, child: const Center());
   }
 }
